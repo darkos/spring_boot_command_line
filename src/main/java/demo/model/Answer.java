fbf9f9a2fefeb28c306c8be075ff1547c85d42cf
Darko Stefanovic <stefanovic.darko@gmail.com>
2015-08-11 22:10:49 -0500

    Question and Answer

---
+++ src/main/java/demo/model/Answer.java
@@ -0,0 +1,10 @@
+package demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Answer {
+	
+}
---
+++ src/main/java/demo/model/Question.java
@@ -0,0 +1,42 @@
+package demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Question {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String title;
+	private String text;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", title=" + title + "]";
+	}
+
+}
